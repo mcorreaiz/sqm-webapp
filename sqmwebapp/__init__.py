e145aaf5a2bae43411b66261af1c9d0c90bfa1be
Matias Correa <mjcorrea5@uc.cl>
2018-07-10 13:52:03 -0400

    Create models

--- sqmwebapp/__init__.py
+++ sqmwebapp/__init__.py
@@ -3,6 +3,16 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_mongoengine import MongoEngine
+
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+db = MongoEngine(app)
+app.config['MONGODB_DB'] = 'project1'
+app.config['MONGODB_HOST'] = '192.168.1.35'
+app.config['MONGODB_PORT'] = 12345
+app.config['MONGODB_USERNAME'] = 'webapp'
+app.config['MONGODB_PASSWORD'] = 'pwd123'
 
 import sqmwebapp.views
---
+++ sqmwebapp/models.py
@@ -0,0 +1,29 @@
+from . import db
+
+class Usuario(db.Document):
+    nombre = db.StringField(max_length=50)
+    sigla  = db.StringField(max_length=2)
+    token  = db.StringField(max_length=50)
+    email  = db.EmailField()
+
+class Version(db.EmbeddedDocument):
+    fsid     = db.IntField() # id de GridFS
+    redactor = db.ReferenceField(Usuario)
+    fecha    = db.DateTimeField()
+
+class Comentario(db.EmbeddedDocument):
+    contenido = db.StringField()
+    redactor  = db.ReferenceField(Usuario)
+    fecha     = db.DateTimeField()
+
+class Nota(db.Document):
+    num                = db.StringField(max_length=5) # Ej. 25.11
+    nombre             = db.StringField(max_length=50)
+    redactores         = db.ListField(db.ReferenceField(Usuario))
+    aprobadores        = db.ListField(db.ReferenceField(Usuario))
+    comentadores       = db.ListField(db.ReferenceField(Usuario))
+    estados_aprobacion = db.DictField() # {sigla: bool}
+    ultima_version     = db.IntField()
+    versiones          = db.ListField(db.EmbeddedDocumentField(Version))
+    ultimo_comentario  = db.IntField()
+    comentarios        = db.ListField(db.EmbeddedDocumentField(Comentario))
